a0a45ec5a35834e85d65b3c437eeeaf8c47ce254
Logan Bailey <logan@logansbailey.com>
2014-07-28 20:26:13 -0700

    Fixes bug in User presenter Attempted to use incorrect User attribute

--- app/Model/User/Presenter.php
+++ app/Model/User/Presenter.php
@@ -14,6 +14,6 @@ class Presenter extends AbstractPresenter
 
     public function full_name()
     {
-        return $this->model->first_name . ' ' . $this->model->Last_name;
+        return $this->model->first_name . ' ' . $this->model->last_name;
     }
 } 
