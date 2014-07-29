a6b4aaa7c36091246807a87c82355ccc2c232f0a
Logan Bailey <logan@logansbailey.com>
2014-07-29 08:09:06 -0700

    User Valdiator A validator class for new users.

---
+++ app/Service/Validator/User/Validator.php
@@ -0,0 +1,27 @@
+<?php namespace Task\Service\Validator\User;
+
+use Illuminate\Validation\Factory;
+
+class Validator
+{
+    /** @var \Illuminate\Validation\Factory  */
+    protected $factory;
+
+    protected $rules = [
+        'first_name' => 'required|min:2|max:255',
+        'last_name' => 'required|min:2|max:255',
+        'email' => 'required|email|unique:users'
+    ];
+
+    public function __construct(Factory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function getErrors(array $input)
+    {
+        $validator = $this->factory->make($input, $this->rules);
+
+        return $validator->fails() ? $validator->messages() : false;
+    }
+} 
