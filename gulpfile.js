a1c7f86420ef8a27b68ff0f61199a7783eec0700
Logan Bailey <logan@logansbailey.com>
2014-08-13 07:14:54 -0700

    New Assets

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,44 @@ var autoprefix = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-var sassDir = 'app/assets/sass';
-var targetCSSDir = 'public/css';
+var sassConfig = {
+    targetDirectory : 'public/css',
+    watchFiles: [
+        'app/assets/sass/main.scss',
+        'app/assets/sass/modules/*',
+    ]
+};
+
 var jsDir = ['app/assets/js/jScript.js', 'app/assets/js/modules/*'];
 var targetJsDir = 'public/js';
 
+var bootstrap = 'app/assets/sass/bootstrap';
+
 
 gulp.task('css', function() {
-    return gulp.src(sassDir + '/main.sass')
-        .pipe(sass({ style: 'compressed' })).on('error', gutil.log)
+    return gulp.src(sassConfig.watchFiles)
+        .pipe(sass({ style: 'compressed' })) //.on('error', gutil.log)
         .pipe(autoprefix('last 10 versions'))
-        .pipe(gulp.dest(targetCSSDir));
+        .pipe(gulp.dest(sassConfig.targetDirectory));
+});
+
+gulp.task('bootstrap', function() {
+    return gulp.src(bootstrap)
+        .pipe(sass({ style: 'compressed' })) //.on('error', gutil.log)
+        .pipe(autoprefix('last 10 versions'))
+        .pipe(gulp.dest(sassConfig.targetDirectory));
 });
 
 gulp.task('js', function() {
     return gulp.src(jsDir)
         .pipe(concat('master.js'))
-        .pipe(uglify())
+//        .pipe(uglify())
         .pipe(gulp.dest(targetJsDir))
 });
 
 gulp.task('watch', function() {
-    gulp.watch(jsDir, ['js']);
-    gulp.watch(sassDir, ['css']);
+    gulp.watch(jsDir + '/*', ['js']);
+    gulp.watch(sassConfig.watchFiles, ['css']);
 });
 
-gulp.task('default', ['css', 'js', 'watch']);
+gulp.task('default', ['css', 'js', 'bootstrap', 'watch']);
