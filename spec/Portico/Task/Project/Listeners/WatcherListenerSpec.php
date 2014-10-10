eee17ffe75db7858fce70dbc64332451a8b21aed
Logan Bailey <logan@logansbailey.com>
2014-10-10 00:31:27 -0700

    WatcherListener Test Rewrites with Mock
    Mocks out the ticket and project to create simpler tests.

--- spec/Portico/Task/Project/Listeners/WatcherListenerSpec.php
+++ spec/Portico/Task/Project/Listeners/WatcherListenerSpec.php
@@ -2,10 +2,8 @@
 
 namespace spec\Portico\Task\Project\Listeners;
 
-use Illuminate\Database\Connection;
 use Laracasts\Commander\Events\EventListener;
 use PhpSpec\ObjectBehavior;
-use Portico\Core\PhpSpec\Relatable;
 use Portico\Task\Project\Project;
 use Portico\Task\Ticket\Events\TicketWasCreated;
 use Portico\Task\Ticket\Ticket;
@@ -15,46 +13,32 @@ use Portico\Task\Project\Listeners\WatcherListener;
 
 class WatcherListenerSpec extends ObjectBehavior
 {
-    use Relatable;
-
-    public function let(Connection $connection)
-    {
-        $this->relate($connection);
-    }
-
     function it_is_initializable()
     {
         $this->shouldHaveType(WatcherListener::class);
         $this->beAnInstanceOf(EventListener::class);
     }
 
-    protected function build_ticket(Project $project)
+    function it_should_attach_the_reporter(Project $project, Ticket $ticket)
     {
-        $ticket = new Ticket();
-        $ticket->setProject($project->getWrappedObject());
-        $ticket->setReporter(new User);
-        $ticket->removeAssignee();
-
-        return $ticket;
-    }
-
-    function it_should_attach_the_reporter(Project $project)
-    {
-        $ticket = $this->build_ticket($project);
+        $ticket->getAttribute('project')->willReturn($project);
+        $ticket->getAttribute('reporter')->willReturn(new User);
+        $ticket->getAttribute('assignee')->willReturn(false);
 
         $project->addWatcher($ticket->reporter)->shouldBeCalled();
 
-        $this->whenTicketWasCreated(new TicketWasCreated($ticket));
+        $this->whenTicketWasCreated(new TicketWasCreated($ticket->getWrappedObject()));
     }
 
-    function it_should_attach_the_assignee(Project $project)
+    function it_should_attach_the_assignee(Project $project, Ticket $ticket)
     {
-        $ticket = $this->build_ticket($project);
-        $ticket->setAssignee(new User);
+        $ticket->getAttribute('project')->willReturn($project);
+        $ticket->getAttribute('reporter')->willReturn(new User);
+        $ticket->getAttribute('assignee')->willReturn(new User);
 
         $project->addWatcher($ticket->reporter)->shouldBeCalled();
         $project->addWatcher($ticket->assignee)->shouldBeCalled();
 
-        $this->whenTicketWasCreated(new TicketWasCreated($ticket));
+        $this->whenTicketWasCreated(new TicketWasCreated($ticket->getWrappedObject()));
     }
 }
--- spec/Portico/Task/Ticket/Listeners/WatcherListenerSpec.php
+++ spec/Portico/Task/Ticket/Listeners/WatcherListenerSpec.php
@@ -1,8 +1,8 @@
 <?php namespace spec\Portico\Task\Ticket\Listeners;
 
-use Illuminate\Database\Connection;
+use Laracasts\Commander\Events\EventListener;
 use PhpSpec\ObjectBehavior;
-use Portico\Core\PhpSpec\Relatable;
+use Portico\Task\Ticket\Events\TicketWasCreated;
 use Portico\Task\Ticket\Listeners\WatcherListener;
 use Portico\Task\Ticket\Ticket;
 use Portico\Task\User\User;
@@ -10,52 +10,30 @@ use Prophecy\Argument;
 
 class WatcherListenerSpec extends ObjectBehavior
 {
-    use Relatable;
-
-    function let(Connection $conn)
-    {
-        $this->relate($conn);
-    }
-
     function it_is_initializable()
     {
         $this->shouldHaveType(WatcherListener::class);
+        $this->beAnInstanceOf(EventListener::class);
     }
 
-    private function build_ticket($includeAssignee = false)
+    function it_should_attach_the_reporter(Ticket $ticket)
     {
-        $ticket = new Ticket;
-
-        $reporter = new User();
-        $reporter->id = 8;
-        $ticket->setReporter($reporter);
-
-        if (!$includeAssignee) {
-            return $ticket;
-        }
-
-        $assignee = new User();
-        $assignee->id = 32;
-        $ticket->setAssignee($assignee);
-    }
-
-    function it_should_attach_the_reporter()
-    {
-        $ticket = $this->build_ticket();
+        $ticket->getAttribute('reporter')->willReturn(new User);
+        $ticket->getAttribute('assignee')->willReturn(null);
 
         $ticket->addWatcher($ticket->reporter)->shouldBeCalled();
 
-        $this->whenTicketWasCreated(new TicketWasCreated($ticket));
+        $this->whenTicketWasCreated(new TicketWasCreated($ticket->getWrappedObject()));
     }
 
-//    function it_should_attach_the_assignee(Project $project)
-//    {
-//        $ticket = $this->build_ticket($project);
-//        $ticket->setAssignee(new User);
-//
-//        $project->addWatcher($ticket->reporter)->shouldBeCalled();
-//        $project->addWatcher($ticket->assignee)->shouldBeCalled();
-//
-//        $this->whenTicketWasCreated(new TicketWasCreated($ticket));
-//    }
+    function it_should_attach_the_assignee(Ticket $ticket)
+    {
+        $ticket->getAttribute('reporter')->willReturn(new User);
+        $ticket->getAttribute('assignee')->willReturn(new User);
+
+        $ticket->addWatcher($ticket->reporter)->shouldBeCalled();
+        $ticket->addWatcher($ticket->assignee)->shouldBeCalled();
+
+        $this->whenTicketWasCreated(new TicketWasCreated($ticket->getWrappedObject()));
+    }
 } 
