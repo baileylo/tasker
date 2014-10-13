2ceeaf1aff3036cd09cb49448c1a20c0224ed93f
Logan Bailey <logan@logansbailey.com>
2014-10-12 22:40:51 -0700

    JScript Dependeny Resolution
    JScript can now automatically resolve dependencies.

--- app/assets/js/jScript.js
+++ app/assets/js/jScript.js
@@ -1,5 +1,6 @@
 var jScript = (function(){
     var modules = {};
+    var executed = {};
 
     var walk = function(nodeList, callback) {
         for (var i = 0, j = nodeList.length; i < j; i++) {
@@ -7,23 +8,40 @@ var jScript = (function(){
         }
     };
 
+    var executeModule = function(moduleName) {
+        if (typeof modules[moduleName] == 'undefined') {
+            console.error('Failed to execute "' + moduleName + '" module. It appears to be undefined.');
+            return;
+        }
+
+        // Resolve Dependencies.
+        walk(modules[moduleName].requirements, function(requiredModule) {
+            executeModule(requiredModule);
+        });
+
+        if (typeof executed[moduleName] != 'undefined') {
+            // prevent double execution of modules.
+            return;
+        }
+
+        modules[moduleName].module();
+    };
 
     return {
         walk: walk,
 
-        add: function(name, module) {
-            modules[name] = module;
+        add: function(name, module, requirements) {
+
+            // force requirements to be an array.
+            if (typeof requirements == 'undefined' || !requirements instanceof Array) {
+                requirements = [];
+            }
+
+            modules[name] = {module: module, requirements: requirements};
         },
 
         exec: function(moduleNames) {
-            walk(moduleNames, function(name) {
-                if (typeof modules[name] != 'function') {
-                    console.error('Failed to execute "' + name + '" module. It appears to be undefined.');
-                    return;
-                }
-
-                modules[name]();
-            });
+            walk(moduleNames, executeModule);
 
         }
     };
