3a2f64cacb8738bbcb792821ac7c203f690ab829
Logan Bailey <logan@logansbailey.com>
2014-10-30 20:37:05 -0700

    Add Date Picker to Edit Ticket Page

---
+++ app/assets/js/pages/viewTicket.js
@@ -0,0 +1,8 @@
+/**
+ * Adds jquery bindings to toggle fields on clicking them
+ */
+jScript.add('viewTicket', function() {
+    $('.datepicker').datepicker({
+        'format': "yyyy-mm-dd"
+    });
+});
